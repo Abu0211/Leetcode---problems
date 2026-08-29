a54175fd1d149da884360576ef31d8db06e9757a
Abu0211 <mohamedabdullaa02@gmail.com>
2026-08-29 09:27:23 +0530

    Time: 0 ms (100%), Space: 19 MB (99.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,14 +1,12 @@
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
 
-        # If input is empty, return empty list
         if not digits:
             return []
 
-        # Phone keypad mapping
         phone = {
-            "0": "",
-            "1": "",
+            "0":"",
+            "1":"",
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -23,23 +21,20 @@ class Solution:
         path = []
 
         def backtrack(index):
-            # Base case: all digits are processed
+            # Base case
             if index == len(digits):
                 result.append("".join(path))
                 return
 
-            # Get letters corresponding to current digit
             letters = phone[digits[index]]
 
-            # Try every possible letter
             for letter in letters:
-                path.append(letter)        # Choose
+                path.append(letter)
 
-                backtrack(index + 1)      # Explore
+                backtrack(index + 1)
 
-                path.pop()                # Unchoose (Backtrack)
+                path.pop()
 
-        # Start from first digit
         backtrack(0)
 
         return result
